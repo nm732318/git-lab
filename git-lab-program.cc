1c61bf177d7f4b6d27385b2edeef6b8e086f96d2
Nicholas McGuigan <nm732318@ohio.edu>
2019-05-16 02:56:18 +0100

    Modifed git-lab-program.cc to read Hello Git!

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Nicholas McGuigan   
+ *        Date: 5/15/2019
+ * Description: "Print Hello Git!"
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl; 
+    return 0;
+}// main
